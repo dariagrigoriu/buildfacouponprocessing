7428ce6817a5e2357d432c7edbaf818a3d38749f
Daria Grigoriu <d.grigoriu@live.com>
2017-05-10 19:43:24 -0700

    deployment slots demo

--- BuildCouponLibrary/CouponWebhook/CouponHandler.cs
+++ BuildCouponLibrary/CouponWebhook/CouponHandler.cs
@@ -25,7 +25,7 @@ namespace BuildCouponLibrary
             String couponValue = null;
             if (data.time == null)
             {
-                couponValue = "No coupon offer available.";
+                couponValue = "No coupon offer available. Please check back later";
             }
             else
             {
